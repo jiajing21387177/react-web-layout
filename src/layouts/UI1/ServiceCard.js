9b6842dcc3c37abe9a8d2b5f53212c22e5a1bc1c
halo <halo@halo.com>
2023-06-24 03:04:44 +0800

    🌈 style(ServiceCard): Modify <a> tag attr

--- src/layouts/UI1/ServiceCard.js
+++ src/layouts/UI1/ServiceCard.js
@@ -1,20 +1,18 @@
 import React from 'react';
-import { ArrowRightIcon } from '@heroicons/react/24/outline'
+import { ArrowRightIcon } from '@heroicons/react/24/outline';
 
 const ServiceCard = ({ icon, title, content }) => {
+    const itemID = `service-${title.replace(/\s+/g, '-')}`;
+
     return (
-        <div className='flex flex-col basis-full md:basis-1/3 gap-3'>
+        <div id={itemID} className="flex flex-col basis-full md:basis-1/3 gap-3">
             {icon}
-            <h3 className='font-bold'>
-                {title}
-            </h3>
-            <p className='flex-auto'>
-                {content}
-            </p>
+            <h3 className="font-bold">{title}</h3>
+            <p className="flex-auto">{content}</p>
             <div>
-                <a href="#" className="mt-5 text-indigo-400 inline-flex gap-1">
+                <a href={`#${itemID}`} className="mt-5 text-indigo-400 inline-flex gap-1">
                     Learn more
-                    <ArrowRightIcon className="h-4 w-4 self-center"></ArrowRightIcon>
+                    <ArrowRightIcon className="h-4 w-4 self-center" />
                 </a>
             </div>
         </div>
